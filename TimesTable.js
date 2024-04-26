4a1e16814eff6d9517e2842a25c6225f58cc52d3
obanla69 <obanlayinka2323@gmail.com>
2024-04-26 06:52:47 +0100

    add file

---
+++ TimesTable.js
@@ -0,0 +1,9 @@
+for(let number1 = 1; number1 <= 12; number1++){
+
+	for(let number2 = 1; number2 <= 12; number2++){
+
+	process.stdout.write(`${number2} * ${number1} = ${number2 * number1} \t`)
+	}
+
+console.log()
+}
